9e628f63eec03b881d2ee3889a152fbefe6b8ae9
apcs <apcs@mvhs-u528ap03.dcsdk12.org>
2013-09-03 08:11:47 -0600

    counting

--- Loops.java
+++ Loops.java
@@ -6,13 +6,22 @@ public class Loops {
 		while (x <= 100) {  // stopping condition
 			System.out.println(x); //loop action
 			x++;
-		}*/
+		}
 
 		for (int i=1; i <= 100; i++) {
 			System.out.println(i);
-		}
+		}*/
 
 		//after
+		/*int x = 45;
+		while (x >= 20) {
+			System.out.println(x);
+			x = x - 5;
+		}*/
+
+		for (int i = 24; i <= 68; i = i + 2) {
+			System.out.println(i);
+		}
 
 	}
 
