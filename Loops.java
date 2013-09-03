93159742234028a50f39aee3df0df6eaac2ec7b9
apcs <apcs@mvhs-u528ap03.dcsdk12.org>
2013-09-03 08:04:17 -0600

    Count to 100

---
+++ Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+	public static void main(String[] args) {
+		
+
+		int x = 1; // start
+		while (x <= 100) {  // stopping condition
+			System.out.println(x); //loop action
+			x++;
+		}
+
+		//after
+
+	}
+
+}
