7d643a5c76afd454dbeaa6ff4accdde4a6b21538
Ryan Zenoni <Ryan@Lisa-Zenonis-MacBook-Pro.local>
2013-09-04 23:12:16 -0600

    hailstone

--- Loops.java
+++ Loops.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Loops {
 	public static void main(String[] args) {
-		
+		hailstone();
 
 		/*int x = 1; // start
 		while (x <= 100) {  // stopping condition
@@ -10,19 +12,39 @@ public class Loops {
 
 		for (int i=1; i <= 100; i++) {
 			System.out.println(i);
-		}*/
+		}
 
 		//after
 		int z = 45;
 		while (z >= 20) {
-			System.out.println(x);
+			System.out.println(z);
 			z = z - 5;
 		}
 
 		for (int i = 24; i <= 68; i = i + 2) {
 			System.out.println(i);
-		}
+		}*/
 
+	}
+	
+	public static void hailstone() {
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Enter number: ");
+		int n = reader.nextInt();
+		while (n != 1) {
+			if (n%2 == 1) {
+				n = n*3 +1;
+				System.out.println(n);
+			}
+			if (n%2 == 0) {
+				n = n/2;
+				System.out.println(n);
+			}
+		}
+		
+		
+		
 	}
 
 }
